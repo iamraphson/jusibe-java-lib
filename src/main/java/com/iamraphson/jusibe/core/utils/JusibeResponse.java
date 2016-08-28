d8b78171dc67021efe8c62f7a104575dea653448
Ayeni Olusegun <nsegun5@gmail.com>
2016-08-28 13:42:01 +0100

    create jusibe response class

---
+++ src/main/java/com/iamraphson/jusibe/core/utils/JusibeResponse.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.iamraphson.jusibe.core.utils;
+
+/**
+ *
+ * @author Raphson
+ */
+public class JusibeResponse {
+    
+    /**
+     * Response Code
+     * @var int
+     */
+    private int responseCode;
+    
+    
+    /**
+     * Response Message
+     * @var String
+     */
+    private String responseMessage;
+
+    public JusibeResponse() {
+    }
+
+    
+    public JusibeResponse(int responseCode, String responseMessage) {
+        this.responseCode = responseCode;
+        this.responseMessage = responseMessage;
+    }
+    
+    /**
+     * Return Response Code 
+     * @return 
+     */
+    public int getResponseCode() {
+        return responseCode;
+    }
+
+    /**
+     * Set Response Code 
+     * @param responseCode 
+     */
+    public void setResponseCode(int responseCode) {
+        this.responseCode = responseCode;
+    }
+
+    /**
+     * Return Response Message 
+     * @return 
+     */
+    public String getResponseMessage() {
+        return responseMessage;
+    }
+
+    /**
+     * Set Response Message 
+     * @param responseMessage
+     */
+    public void setResponseMessage(String responseMessage) {
+        this.responseMessage = responseMessage;
+    }
+    
+}
