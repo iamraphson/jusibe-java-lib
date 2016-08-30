05f521f6784ce36e1a664256d555420320b22be0
Ayeni Olusegun <nsegun5@gmail.com>
2016-08-30 04:47:22 +0100

    Create lib test

---
+++ src/test/java/com/iamraphson/jusibe/core/JusibeTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.iamraphson.jusibe.core;
+
+import com.iamraphson.jusibe.core.exceptions.IsNullException;
+import com.iamraphson.jusibe.core.utils.JusibeResponse;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ *
+ * @author Raphson
+ */
+public class JusibeTest {
+    
+    @Test(expected = Exception.class)
+    public void testNothingWasNotPassedToJusibeConstructor() throws Exception {
+        new Jusibe(null, null);
+    }
+    
+    @Test
+    public void testGet() throws MalformedURLException , IsNullException, IOException {
+        Jusibe client = new Jusibe(
+                        "XXXXXXXXXXXXX",
+                        "XXXXXXXXXXXXX");
+        
+        JusibeResponse response = client.checkAvailableCredits();
+        assertEquals(200, response.getResponseCode());
+    }
+    
+    @Test
+    public void testClassInstance() throws MalformedURLException , IsNullException {
+        Jusibe client = new Jusibe(
+                        "XXXXXXXXXXXXX",
+                        "XXXXXXXXXXXXX");
+        
+        assert(client instanceof Jusibe);
+    }
+    
+    @Test
+    public void testGetWith401Error() throws MalformedURLException , IsNullException, IOException {
+        Jusibe client = new Jusibe(
+                        "XXXXXXXXXXXXX",
+                        "XXXXXXXXXXXXXX");
+        
+        JusibeResponse response = client.checkAvailableCredits();
+        assertEquals(401, response.getResponseCode());
+    }
+    
+}
